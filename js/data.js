a95d0f1d218c3dd4cc7d17c841016ec8de9062d7
drmoonland <moonland.tech@gmail.com>
2025-08-02 14:05:52 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,269 @@
+let data = [{
+        'id': 1,
+        'name': 'Name Creator ForPubg',
+        'img': './img/app/app_7.png',
+        'url': 'https://play.google.com/store/apps/details?id=com.moonlandtech.pubgrenametool&hl=en',
+        'platform': 'Android',
+        'details': 'Cool fonts, fancy symbols User nickname making app.',
+        'technology': 'App / Android Native',
+        'screens': [
+            './img/screens/1/Screen1.png',
+            './img/screens/1/Screen2.png',
+            './img/screens/1/Screen3.png',
+        ],
+    },
+    {
+        'id': 2,
+        'name': 'Ball Run',
+        'img': './img/app/app_14.png',
+        'technology': 'Apple, Ios',
+        'url': 'https://play.google.com/store/apps/details?id=com.Beyoundlimit.ElectricBall2017&hl=en',
+        'platform': 'Android',
+        'technology': 'Game / Unity3d-Arcade Game',
+        'details': 'Simple Arcade Game with Play games achievements , leader board , Admob Ads',
+        'screens': [
+            './img/screens/2/Screen1.png',
+            './img/screens/2/Screen2.png',
+        ],
+    },
+    {
+        'id': 3,
+        'name': 'Star Soccer league',
+        'img': './img/app/app_6.png',
+        'url': 'https://play.google.com/store/apps/details?id=com.bitpixlab.soccerleague&hl=en',
+        'platform': 'Android',
+        'technology': 'Game / Unity3d-Online Multiplayer-Local Multiplayer',
+        'details': 'Online multiplayer game up to 6 players , photon network , playgames achievements , leaderboard , multi languages , Car customization , LAN multiplayer using UNET.',
+        'screens': [
+            './img/screens/3/Screen1.png',
+            './img/screens/3/Screen2.png',
+        ],
+    },
+    {
+        'id': 4,
+        'name': 'Super RocketBall',
+        'img': './img/app/app_5.png',
+        'url': 'https://play.google.com/store/apps/details?id=com.digitalpixelproductions.rocketball&hl=en',
+        'platform': 'Android / IOS',
+        'technology': 'Game / Unity3d-Online Multiplayer',
+        'details': 'Online multiplayer game up to 6 players , photon network , playgames achievements , leaderboard.',
+        'screens': [
+            './img/screens/4/Screen1.png',
+            './img/screens/4/Screen2.png',
+        ],
+    },
+    {
+        'id': 5,
+        'name': 'Air Combat',
+        'img': './img/app/app_15.png',
+        'url': 'https://apps.apple.com/us/app/id1184828270?mt=8',
+        'platform': 'Android / IOS',
+        'technology': 'Game / Unity3d-Online Multiplayer-Fighter plane simulation',
+        'details': 'Online multiplayer game up to 6 players, photon network, playgamesachievements , leaderboard.',
+        'screens': [
+            './img/screens/5/Screen1.png',
+            './img/screens/5/Screen2.png',
+        ],
+    },
+    {
+        'id': 6,
+        'name': 'Battleground : Combat & Domination',
+        'img': './img/app/app_13.png',
+        'url': 'https://play.google.com/store/apps/details?id=com.thundersoft.battleground1&hl=en',
+        'platform': 'Android / IOS',
+        'technology': 'Game / Unity3d-Third person shooter',
+        'details': 'Battleground is a realistic multiplayer 3D-Top Down Shooter game. Game has 3 different types of maps : Hospital, Industry, Forest. You can fight alone in a free for all arena, with your friends or in an open game versus players around the world. Or join a live Battleground Game, team versus team to overcome your enemy and claim victory. Two type of Squads to join either Cobra Squad or Omega Squad.',
+        'screens': [
+            './img/screens/6/Screen1.png',
+            './img/screens/6/Screen2.png',
+        ],
+    },
+    {
+        'id': 7,
+        'name': 'Color Panda',
+        'img': './img/app/app_12.png',
+        'url': 'https://play.google.com/store/apps/details?id=com.thundersoft.puzzle.colorpanda&hl=en',
+        'platform': 'Android / IOS',
+        'technology': 'Game/Unity3d-puzzle',
+        'details': 'A Color Panda is challenging 30 level color puzzle game . Your task is to make color of panda as displayed on corner of game. This game contains 6 different props to cover part of panda face , while dropping panda on any color pot its color will not effect covered part',
+        'screens': [
+            './img/screens/7/Screen1.png',
+            './img/screens/7/Screen2.png',
+        ]
+
+    },
+    {
+        'id': 8,
+        'name': '16K - The 2048 Game',
+        'img': './img/app/app_1.png',
+        'url': 'https://play.google.com/store/apps/details?id=com.thundersoft.game2048.resolution16k&hl=en',
+        'platform': 'Android / IOS',
+        'technology': 'Game / Unity3d-puzzle',
+        'details': 'This game is The classic 2048 puzzle is a fun, addictive and a very simple resolution puzzle game. You join the resolution and get to the 16K tile!',
+        'screens': [
+            './img/screens/8/Screen1.png',
+            './img/screens/8/Screen2.png',
+        ]
+    },
+    {
+        'id': 9,
+        'name': 'Moto Racing Driver',
+        'img': './img/app/app_10.png',
+        'url': 'https://play.google.com/store/apps/details?id=com.tss.moto.racing.driver&hl=en',
+        'platform': 'Android / IOS',
+        'technology': 'Game / Unity3d-Bike racing',
+        'details': 'Moto racing driver is an endless traffic moto racing game. Drive your moto (motorcycle) on busy road in city or forest with furious speed and collect as many coins as you can. Challenge others, score high and be the champion driver of moto racing. It brings you stunning graphics, smooth control, beautiful view (forest & city area) and most compelling traffic experience ever.',
+        'screens': [
+            './img/screens/9/Screen1.png',
+            './img/screens/9/Screen2.png',
+            './img/screens/9/Screen3.png',
+            './img/screens/9/Screen4.png',
+        ]
+    },
+    {
+        'id': 10,
+        'name': 'MoonLand WDS',
+        'img': './img/app/app_2.png',
+        'url': '',
+        'platform': 'Windows OS',
+        'technology': 'Software / WindowsVB.Net-Database-Printin-Backups',
+        'details': 'Record keeping / Billing / ledger software for single user.Customer details, Order Details, monthly reports. PDF print.',
+        'screens': [
+            './img/screens/10/Screen1.png',
+        ],
+    },
+    {
+        'id': 11,
+        'name': 'Lable2Image',
+        'img': './img/app/app_11.png',
+        'url': '',
+        'platform': 'Windows OS',
+        'technology': 'Software / Windows VB.Net-Backups',
+        'details': 'Image processing software. Add Records to images . Image gallery .Lable images using different colors , fonts , sizes , location.',
+        'screens': [
+            './img/screens/11/Screen1.png',
+        ],
+    },
+    {
+        'id': 12,
+        'name': 'Vprinter-Internal',
+        'img': './img/app/app_3.png',
+        'url': '',
+        'platform': 'Windows OS',
+        'technology': 'Internal Software / Pulg-in/Windows VB.Net',
+        'details': 'Print output management software.Number of copies , print location , mirror print ,move image on paper etc. This software is part of other software.',
+        'screens': [
+            './img/screens/12/Screen1.png',
+        ],
+    },
+    {
+        'id': 13,
+        'name': 'MugAnimator',
+        'img': './img/app/app_9.png',
+        'url': 'http://www.muganimator.com/',
+        'platform': 'Windows OS / Mac OS',
+        'technology': 'Software/Windows / Mac Unity3d',
+        'details': 'Mockups maker 3d software , Over 60 items like t-shirts, mugs ,photo frames ,kitchens etc. Digital license online activation . Free and pro version of software. Multi language support Built in image editor , Bill pdf maker , watermark placer . HD Photo Video GIF output Fast GPU image processing',
+        'screens': [
+            './img/screens/13/Screen1.png',
+            './img/screens/13/Screen2.png',
+        ],
+    },
+];
+
+function generateStructuredData() {
+    const structuredData = {
+        "@context": "https://schema.org",
+        "@type": "ItemList",
+        "itemListElement": data.map((item, index) => ({
+            "@type": "ListItem",
+            "position": index + 1,
+            "item": {
+                "@type": "Product",
+                "name": item.name,
+                "description": item.details,
+                "image": item.img,
+                "url": item.url || window.location.href,
+                "operatingSystem": item.platform,
+                "applicationCategory": item.technology
+            }
+        }))
+    };
+
+    const script = document.createElement('script');
+    script.type = 'application/ld+json';
+    script.textContent = JSON.stringify(structuredData);
+    document.head.appendChild(script);
+}
+
+$(document).ready(function() {
+    var text = '',
+        li = '',
+        j = 0;
+
+    $.each(data, function(index, item) {
+
+        if (index % 4 == 0) {
+            text += '';
+            li = '<li class="" data-target="#da-carouselIndicator" data-slide-to="' + j + '"></li>';
+        }
+
+        text += '<div class="card d-block mb-3"> <img class="card-img-top" src="' + item.img + '" alt="image"/> <div class="card-body text-center"> <div class="h5">' + item.name + '</div><p class="card-text">' + item.details.substring(0, 40) + '...' + '</p><p class="text-color1">' + item.platform + '</p><a href="javascript:void(0);" data-id="' + item.id + '" class="btn btn-sm btn-primary btn-more">View more</a> </div></div>';
+
+        if ((index != 0 && (index + 1) % 4 == 0) || (index == data.length - 1)) {
+            j++;
+            // $("#carousel-indicators").append(li);
+            text += '</div>';
+            $('#slick-inner').append(text);
+            text = "";
+        }
+    });
+
+    $('.carousel-item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
+    // $('#carousel').carousel();
+
+    generateStructuredData();
+});
+$(document).on('click', '.btn-more', function() {
+
+    var id = $(this).attr('data-id');
+    var app = '';
+    $.each(data, function(index, item) {
+        if (item.id == id)
+            app = item;
+    });
+
+    if (app) {
+        var _modalCarousel = '';
+
+        var screens = [
+            './img/screens/1/screen1.png',
+            './img/screens/1/screen2.png',
+            './img/screens/1/screen3.png',
+        ];
+
+        var screens = app.screens || screens;
+
+        if (screens) {
+            $.each(screens, function(index, item) {
+                _modalCarousel += '<div class="carousel-item"><img class="d-block w-100" src="' + item + '" alt="First slide"></div>';
+            });
+
+            $('#_modal_carousel').empty();
+            $('#_modal_carousel').append(_modalCarousel);
+        }
+
+        $('#_modal_carousel').find('.carousel-item').first().addClass('active');
+
+        $("#modalQuickView").find('#_modal_app_title, ._modal_app_title').text(app.name);
+        $("#modalQuickView").find('#_modal_app_details').text(app.details);
+        $("#modalQuickView").find('#_modal_app_platform').text(app.platform);
+        $("#modalQuickView").find('#_modal_app_tech').text(app.technology);
+
+        if (app.url)
+            $("#modalQuickView").find('#btn-block').html('<a target="_blank" class="btn btn-info btn-site btn-md waves-effect waves-light" href="' + app.url + '">Read more</a>');
+
+        $("#modalQuickView").modal('show');
+    }
+});
